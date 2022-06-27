d8577f54a979e0b4e3dc236f3dd127e24b8a2321
jeugeniojunior <jeugenio.junior@gmail.com>
2022-06-27 06:29:34 -0300

    Corrigido localização do Keys.

--- autozap.py
+++ autozap.py
@@ -1,6 +1,5 @@
-import time
-
 import pandas as pd
+import time
 
 contatos_df = pd.read_excel('date.xlsx')
 print(contatos_df)
@@ -21,10 +20,13 @@ while len(navegador.find_elements(By.ID,"side")) < 1:
 for i, mensagem in enumerate(contatos_df['mensagem']):
     pessoa = contatos_df.loc[i, "pessoa"]
     numero = contatos_df.loc[i, "numero"]
-    texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem}")
+    formulario = 'https://forms.gle/cg1dnuNrufe4AsNK8'
+    texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem} {formulario}")
     link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
     navegador.get(link)
-    while len(navegador.find_elements(By.ID,"side")) < 1:
+    while len(navegador.find_elements(By.ID, "side")) < 1:
         time.sleep(1)
-    navegador.find_element(By.XPATH,'// *[ @ id = "main"] / footer / div[1] / div / span[2] / div / div[2] / div[1] / div / div[2]').send_keys(Keys.ENTER)
+    navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
     time.sleep(10)
+
+
