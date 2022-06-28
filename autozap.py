3c2e95877d881a060b8adcf3d465ed40fa145832
jeugeniojunior <jeugenio.junior@gmail.com>
2022-06-28 06:56:26 -0300

    Corrigido localização do Keys.

--- autozap.py
+++ autozap.py
@@ -1,32 +1,38 @@
 import pandas as pd
 import time
-
-contatos_df = pd.read_excel('date.xlsx')
-print(contatos_df)
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import urllib
 from selenium.webdriver.common.by import By
 
-navegador = webdriver.Chrome()
-navegador.get('https://web.whatsapp.com/')
+def enviar():
+    contatos_df = pd.read_excel('date.xlsx')
+    print(contatos_df)
 
-while len(navegador.find_elements(By.ID,"side")) < 1:
-    time.sleep(1)
+    navegador = webdriver.Chrome()
+    navegador.get('https://web.whatsapp.com/')
 
-# já estamos com o login feito no whatsapp web
-for i, mensagem in enumerate(contatos_df['mensagem']):
-    pessoa = contatos_df.loc[i, "pessoa"]
-    numero = contatos_df.loc[i, "numero"]
-    formulario = 'https://forms.gle/cg1dnuNrufe4AsNK8'
-    texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem} {formulario}")
-    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
-    navegador.get(link)
-    while len(navegador.find_elements(By.ID, "side")) < 1:
+    while len(navegador.find_elements(By.ID,"side")) < 1:
         time.sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
-    time.sleep(10)
+        print('Aguardando Login')
 
+    print('Login Efetuado com sucesso!')
+
+    # já estamos com o login feito no whatsapp web
+    for i, mensagem in enumerate(contatos_df['mensagem']):
+        pessoa = contatos_df.loc[i, "pessoa"]
+        numero = contatos_df.loc[i, "numero"]
+        formulario = 'https://forms.gle/cg1dnuNrufe4AsNK8'
+        texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem} {formulario}")
+        link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+        navegador.get(link)
+        while len(navegador.find_elements(By.ID, "side")) < 1:
+            time.sleep(1)
+        navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span').click()
+        print('Enviada mensagem com sucesso!')
+        time.sleep(10)
+    print('Fim do Processo!')
+
+enviar()
 
